c0b1a677ac0482f4c3a4fdf944b31744afbe361a
NipunSharma2902 <sharmanipun2902@gmail.com>
2022-05-28 19:30:37 +0530

    whatever

--- lib/presentation/screens/journal.dart
+++ lib/presentation/screens/journal.dart
@@ -1,4 +1,5 @@
 import 'package:anavrin/presentation/screens/journal.dart';
+import 'package:anavrin/presentation/screens/my_homepage.dart';
 import 'package:animate_do/animate_do.dart';
 //import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:date_picker_timeline/date_picker_timeline.dart';
@@ -22,6 +23,7 @@ import 'package:anavrin/shared/constants/consts_variables.dart';
 import 'package:anavrin/shared/constants/strings.dart';
 import 'package:anavrin/shared/services/notification_service.dart';
 import 'package:anavrin/shared/styles/colors.dart';
+import 'package:anavrin/presentation/screens/my_homepage.dart';
 
 
 
@@ -39,6 +41,17 @@ import 'package:anavrin/presentation/widgets/myindicator.dart';
 import 'package:anavrin/presentation/widgets/mysnackbar.dart';
 import 'package:anavrin/shared/constants/consts_variables.dart';
 
+
+
+ int pageIndex = 0;
+  
+  final pages = [
+    const MyHomePage(),
+    //const (),
+    journal(),
+  ];
+
+
 class journal extends StatefulWidget {
   journal({Key? key}) : super(key: key);
 
@@ -50,139 +63,134 @@ class _journalState extends State<journal> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:MultiBlocListener( listeners: [
-          BlocListener<ConnectivityCubit, ConnectivityState>(
-              listener: (context, state) {
-            if (state is ConnectivityOnlineState) {
-              MySnackBar.error(
-                  message: 'You Are Online Now ',
-                  color: Colors.green,
-                  context: context);
-            } else {
-              MySnackBar.error(
-                  message: 'Please Check Your Internet Connection',
-                  color: Colors.red,
-                  context: context);
-            }
-          }),
-          BlocListener<AuthenticationCubit, AuthenticationState>(
-            listener: (context, state) {
-              if (state is UnAuthenticationState) {
-                Navigator.pushNamedAndRemoveUntil(
-                    context, welcomepage, (route) => false);
-              }
-            },
-          )
+      appBar: AppBar(
+        leading: Icon(
+          Icons.book_online,
+          color: Theme.of(context).primaryColor,
+        ),
+
+        title: const Text(
+          "Journal Mode",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+          ),
+        ),
+        centerTitle: true,
+      ),
+        //body: pages[pageIndex],
+        bottomNavigationBar: buildMyNavBar(context),
+      );
+            
+  }
+
+
+
+
+
+
+
+  Container buildMyNavBar(BuildContext context) {
+    return Container(
+      height: 50,
+      margin: const EdgeInsets.only(
+        bottom: 10,
+        left: 50,
+        right: 50,
+        ),
+      decoration: const BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 10,
+            spreadRadius: 5,
+          ),
         ],
-            child: BlocBuilder<AuthenticationCubit, AuthenticationState>(
-              builder: (context, state) {
-                if (state is AuthenticationLoadingState) {
-                  return const MyCircularIndicator();
-                }
-
-
-
-      return SafeArea(
-                    child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          InkWell(
-                            onTap: () {
-                              // NotificationsHandler.createNotification();
-                            },
-                            child: SizedBox(
-                              height: 8.h,
-                              child: Image.asset(
-                                profileimages[profileimagesindex],
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            width: 3.w,
-                          ),
-                          Expanded(
-                            child: Text(
-                              user.displayName != null
-                                  ? 'Hello ${user.displayName}'
-                                  : ' Hello $username',
-                              overflow: TextOverflow.ellipsis,
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headline1!
-                                  .copyWith(fontSize: 15.sp),
-                            ),
-                          ),
-                          InkWell(
-                            onTap: () {
-                              _showBottomSheet(context, authenticationCubit);
-                            },
-                            child: Icon(
-                              Icons.settings,
-                              size: 25.sp,
-                              color: Appcolors.black,
-                            ),
-                          )
-                        ],
-                      ),
-                      SizedBox(
-                        height: 3.h,
-                      ),
-
-
-
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text(
-                            DateFormat('MMMM, dd').format(currentdate),
-                            style: Theme.of(context)
-                                .textTheme
-                                .headline1!
-                                .copyWith(fontSize: 17.sp),
-                          ),
-                          const Spacer(),
-                          MyButton(
-                            color: Colors.deepPurple,
-                            width: 40.w,
-                            title: '+ Add Task',
-                            func: () {
-                              Navigator.pushNamed(
-                                context,
-                                addtaskpage,
-                              );
-                            },
-                          )
-                        ],
-                      ),
-                      SizedBox(
-                        height: 3.h,
-                      ),
-                      _buildDatePicker(context, connectivitycubit),
-                      SizedBox(
-                        height: 4.h,
-                      ),
-                      Expanded(
-                          child: StreamBuilder(
-                        stream: FireStoreCrud().getTasks(
-                          mydate: DateFormat('yyyy-MM-dd').format(currentdate),
-                        ),
-                        builder: (BuildContext context,
-                            AsyncSnapshot<List<TaskModel>> snapshot) {
-                          if (snapshot.hasError) {
-                            return _nodatawidget();
-                          }
-                          if (snapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return const MyCircularIndicator();
-                          }
-
-      
+        color: Color.fromARGB(255, 111, 12, 198),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+          bottomLeft: Radius.circular(30),
+          bottomRight: Radius.circular(30),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          IconButton(
+            enableFeedback: false,
+            onPressed: () {
+              setState(() {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => MyHomePage()),
+                    );
+                pageIndex = 0;
+              });
+            },
+            icon: pageIndex == 0
+                ? const Icon(
+                    Icons.calendar_month,
+                    color: Colors.white,
+                    size: 35,
+                  )
+                : const Icon(
+                    Icons.calendar_month_outlined,
+                    color: Color.fromARGB(255, 21, 170, 234),
+                    size: 35,
+                  ),
+          ),
+          IconButton(
+            enableFeedback: false,
+            onPressed: () {
+              setState(() {
+                pageIndex = 1;
+              });
+            },
+            icon: pageIndex == 1
+                ? const Icon(
+                    Icons.chat_bubble_rounded,
+                    color: Color.fromARGB(255, 21, 170, 234),
+                    size: 35,
+                  )
+                : const Icon(
+                    Icons.chat_bubble_outline_rounded,
+                    color: Colors.white,
+                    size: 35,
+                  ),
+          ),
+          
+          IconButton(
+            enableFeedback: false,
+            
+            onPressed: () {
+              setState(() {
+                
+                pageIndex = 2;
+              });
+            },
+            icon: pageIndex == 2
+                ? const Icon(
+                    Icons.book,
+                    color: Colors.white,
+                    size: 35,
+                  )
+                : const Icon(
+                    Icons.book_outlined,
+                    color: Color.fromARGB(255, 21, 170, 234),
+                    size: 35,
+                  ),
+          ),
+        ],
+      ),
     );
   }
+
+  
+
+
 }
+
+
+
+      
+            
